c82dd2c2b52d7e290c4492197119405558f60424
Tan Nguyen <tannd1993@gmail.com>
2015-06-06 13:12:21 +0700

    post2

--- both/collections/posts.js
+++ both/collections/posts.js
@@ -6,3 +6,46 @@ Posts.helpers({
 Posts.before.insert(function (userId, doc) {
   doc.createdAt = moment().toDate();
 });
+
+
+validatePost = function (post) {
+  var errors = {};
+
+  if (!post.title) {
+    errors.title = "Điền tiêu đề vào đi má.";
+  }
+
+  if (!post.url) {
+    errors.url = "Link youtube đâu má.";
+  }
+
+  return errors;
+}
+
+
+Meteor.methods({
+  postInsert: function (postAttributes) {
+    var postWithSameLink = Posts.findOne({url: postAttributes.url});
+    if (postWithSameLink) {
+      return {
+        postExists: true,
+        _id: postWithSameLink._id
+      };
+    }
+
+    var user = Meteor.user();
+    var post = _.extend(postAttributes, {
+      userId: user._id,
+      username: user.username,
+      postedDate: new Date(),
+      url: postAttributes.url,
+      title: postAttributes.title,
+    });
+
+    var postId = Posts.insert(post);
+
+    return {
+      _id: postId
+    };
+  }
+});
