e3cf48d7c0ceac34b8cbc5ed89a9ed491bfb72ed
Tam Pham <tampham47@live.com>
2015-06-06 13:17:35 +0700

    show user id for each post

--- both/collections/comments.js
+++ both/collections/comments.js
@@ -16,15 +16,18 @@ Meteor.methods({
     //   body: String
     // });
 
-    // var user = Meteor.user();
+    var user = Meteor.user();
+    if (!user) {
+      throw new Meteor.Error('invalid-user', 'You must login to comment!');
+    }
     // var post = Posts.findOne(commentAttributes.postId);
 
     // if (!post)
     //   throw new Meteor.Error('invalid-comment', 'You must comment on a post');
 
-    // comment = _.extend(commentAttributes, {
-    //   submitted: new Date()
-    // });
+    commentModel = _.extend(commentModel, {
+      userId: user._id
+    });
 
     // create the comment, save the id
     var commentId = Comments.insert(commentModel);
